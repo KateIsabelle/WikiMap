283ffae7711dcc3b6f807986ac678f92870e577c
KateIsabelle <kate.davis887@gmail.com>
2021-04-03 23:34:06 +0000

    fix users seeds to include password

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,17 +1,17 @@
 INSERT INTO users (name, email, password)
-VALUES ('Penny Lane', pennylane@hotmail.com);
+VALUES ('Penny Lane', 'pennylane@hotmail.com', 'password');
 
 INSERT INTO users (name, email, password)
-VALUES ('Neville Longbottom', harrypotter4lyfe@gmail.com);
+VALUES ('Neville Longbottom', 'harrypotter4lyfe@gmail.com', 'password');
 
 INSERT INTO users (name, email, password)
-VALUES ('Troy Wigglesworth', wiggley22@gmail.com);
+VALUES ('Troy Wigglesworth', 'wiggley22@gmail.com', 'password');
 
 INSERT INTO users (name, email, password)
-VALUES ('Petunia Ignatus', pettyp@yahoo.ca);
+VALUES ('Petunia Ignatus', 'pettyp@yahoo.ca'), 'password';
 
 INSERT INTO users (name, email, password)
-VALUES ('Cheshire Cat', ches.cat@gmail.com);
+VALUES ('Cheshire Cat', 'ches.cat@gmail.com', 'password');
 
 
 
