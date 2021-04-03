b17d16d74f56201582ec205a2aca346c355d05c1
Ruowen Tang <ruowen.tang@gmail.com>
2021-04-03 19:18:37 -0400

    Add GET and POST routes to the maps

--- routes/maps.js
+++ routes/maps.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
+  // GET /maps/
+  router.get('/', (req, res) => {
     db.query(`SELECT maps.* FROM maps
         ORDER BY created_at;`)
       .then(data => {
@@ -15,5 +16,26 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+  // GET /maps/:map_id  -- Display a map by id
+  router.get('/:map_id', (req, res) => {
+    console.log('==> GET /maps/:map_id  -- Display a map by id');
+  });
+
+  // GET /maps/create -- Create new map
+  router.get('/create', (req, res) => {
+    console.log('==> GET /maps/create -- Create new map');
+  });
+
+  // POST /maps/:map_id/edit -- Edit a map
+  router.post('/:map_id/edit', (req, res) => {
+    console.log('==> POST /maps/:map_id/edit -- Edit a map');
+  });
+
+  // POST /maps/:map_id/delete -- Delete a map
+  router.get('/:map_id/delete', (req, res) => {
+    console.log('==> POST /maps/:map_id/delete -- Delete a map');
+  });
+
   return router;
 };
