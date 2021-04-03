3284d9057c5ee55a8965de81080e47baef39f60d
KateIsabelle <kate.davis887@gmail.com>
2021-04-03 19:08:05 +0000

    update schemas and seeds for users table and favourites table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,17 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, email, password)
+VALUES ('Penny Lane', pennylane@hotmail.com);
+
+INSERT INTO users (name, email, password)
+VALUES ('Neville Longbottom', harrypotter4lyfe@gmail.com);
+
+INSERT INTO users (name, email, password)
+VALUES ('Troy Wigglesworth', wiggley22@gmail.com);
+
+INSERT INTO users (name, email, password)
+VALUES ('Petunia Ignatus', pettyp@yahoo.ca);
+
+INSERT INTO users (name, email, password)
+VALUES ('Cheshire Cat', ches.cat@gmail.com);
+
+
+
