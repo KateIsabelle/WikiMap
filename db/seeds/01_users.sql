648fef957fc083a0cdc34d10ff70c58b2f5eda44
code-driving <tittan7@gmail.com>
2021-04-04 01:54:18 +0000

    Remove bracket

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -8,7 +8,7 @@ INSERT INTO users (name, email, password)
 VALUES ('Troy Wigglesworth', 'wiggley22@gmail.com', 'password');
 
 INSERT INTO users (name, email, password)
-VALUES ('Petunia Ignatus', 'pettyp@yahoo.ca'), 'password';
+VALUES ('Petunia Ignatus', 'pettyp@yahoo.ca', 'password');
 
 INSERT INTO users (name, email, password)
 VALUES ('Cheshire Cat', 'ches.cat@gmail.com', 'password');
