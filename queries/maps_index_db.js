99dbef192f3ac3b3dbaf84bc9313964ec6706cd9
KateIsabelle <kate.davis887@gmail.com>
2021-04-05 17:06:31 +0000

    creating query and ejs rendering of most recent maps db info onto index '/maps' ejs page

--- queries/maps_index_db.js
+++ queries/maps_index_db.js
@@ -1,27 +1,31 @@
 const getFirstMaps = (db) => {
   const query = `
-    SELECT * FROM maps
+    SELECT maps.*, users.name as user_name
+    FROM maps
+    JOIN users ON user_id = users.id
+    WHERE users.id = user_id
     ORDER BY id DESC
     LIMIT 4
     ;`;
   return db
     .query(query)
     .then(data => {
-      const maps = data.rows;
-      const templateVars = {};
-      for (let map of maps) {
-        const m = {
-        id: map.id,
-        title: map.title,
-        image: map.image,
-        latitude: map.latitude,
-        longitude: map.longitude,
-        zoom: map.zoom
-        };
-        templateVars[map.id] = m;
-      }
+      const mapsArray = data.rows;
+      // const newMaps = {};
+      // for (let map of mapsArray) {
+      //   const m = {
+      //   id: map.id,
+      //   title: map.title,
+      //   image: map.image,
+      //   latitude: map.latitude,
+      //   longitude: map.longitude,
+      //   zoom: map.zoom,
+      //   user_name: map.user_name
+      //   };
+      // newMaps[map.id] = m;
+      // }
 
-      return templateVars;
+      return {maps: mapsArray}
     })
     .catch(err => {
       res
