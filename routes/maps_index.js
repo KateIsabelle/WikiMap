4640a10a8c4ee10358922c1ec2ac653c22646abc
KateIsabelle <kate.davis887@gmail.com>
2021-04-06 02:55:50 +0000

    more messing around with index to render pins on maps...Not yet working

--- routes/maps_index.js
+++ routes/maps_index.js
@@ -16,19 +16,28 @@ router.get('/', (req,res) => {
     .then((pinsArray) => {
       templateVars.pins = pinsArray;
       templateVars.apiKey = apiKey;
-      console.log('TemplateVars.pins:', templateVars.pins)
+      console.log('TemplateVars:', templateVars)
       res.render("index", templateVars);
     })
   })
-  .catch(err => {
-    res
-      .status(500)
-      .json({ error: err.message });
-  });
+  // .catch(err => {
+  //   res
+  //     .status(500)
+  //     .json({ error: err.message });
+  // });
 
 });
 
 return router
 
 };
+
+// <% let src = `https://maps.googleapis.com/maps/api/staticmap?center=${map.latitude},${map.longitude}&zoom=13&size=400x200&maptype=roadmap` %>
+//       <% for(let pin of pins) { %>
+//         <% if (pin.map_id === map.id) { %>
+//           <% src += `&markers=color:blue%7Clabel:S%7C${map.latitude},${map.longitude}` %>
+//           <% } %>
+//           <% src += `&key=${apiKey}` %>
+
+
 
