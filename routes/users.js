32441439cc014feeffbb93b7841f2a1fe0d49e3c
code-driving <tittan7@gmail.com>
2021-04-06 16:43:57 +0000

    User profile: route. ejs

--- routes/users.js
+++ routes/users.js
@@ -5,9 +5,11 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
+const { request } = require('express');
 const express = require('express');
 const router  = express.Router();
 const { getUsers } = require('../queries/users_db');
+const dbFns = require('../queries/users_db');
 
 module.exports = (db) => {
 
@@ -23,6 +25,13 @@ module.exports = (db) => {
   // GET /users/:id -- Display user profile
   router.get('/:id', (req, res) => {
     console.log('==> GET /users/:id -- Display user profile');
+    res.send('hello from user')
+    // const user = req.session.user_id;
+   // if (!user) {
+    //   res.redirect("/maps");
+    //   return;
+    // }
+    res.render('user');
   });
 
   return router;
