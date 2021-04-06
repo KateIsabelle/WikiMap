a15701866d2853d856fc6fa0e2a881662b35664b
KateIsabelle <kate.davis887@gmail.com>
2021-04-06 16:57:14 +0000

    clean up code

--- queries/maps_index_db.js
+++ queries/maps_index_db.js
@@ -1,5 +1,6 @@
 const apiKey = process.env.API_KEY;
 
+//Query maps table and accompanying pin info
 const getFirstMaps = (db) => {
   const query = `
       SELECT maps.*, users.name as user_name, array_agg(pins.lat || ',' || pins.lng) as markers
@@ -27,46 +28,6 @@ const getFirstMaps = (db) => {
     })
 };
 
-// get pins
-// const getPins = (db) => {
-//   const query = `
-//     SELECT * FROM pins
-//     ;`;
-//   return db
-//     .query(query)
-//     .then(data => {
-//       return data.rows;
-//     })
-// };
-
 module.exports = { getFirstMaps }
 
 
-// const query = `
-//     SELECT maps.*, users.name as user_name
-//     FROM maps
-//     JOIN users ON user_id = users.id
-//     WHERE users.id = user_id
-//     ORDER BY id DESC
-//     LIMIT 4
-//     ;`;
-//   return db
-//     .query(query)
-//     .then(data => {
-//       const mapsArray = data.rows;
-
-
-//       return mapsArray;
-
-//     })
-//     .catch(err => {
-//       res
-//         .status(500)
-//         .json({ error: err.message });
-//     });
-
-// <% for(let pin of pins) { %>
-//   <% if (pin.map_id === map.id) { %>
-//     <% src += `&markers=color:blue%7Clabel:S%7C${map.latitude},${map.longitude}` %>
-//     <% } %>
-//     <% src += `&key=${apiKey}` %>
