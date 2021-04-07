fe6fcfb66e2dffd3e1f7bd2e467a0f6a67db48eb
KateIsabelle <kate.davis887@gmail.com>
2021-04-07 01:01:13 +0000

    add pseudocode into queries/map_index_db.js

--- queries/maps_index_db.js
+++ queries/maps_index_db.js
@@ -2,6 +2,7 @@ const apiKey = process.env.API_KEY;
 
 //Query maps table and accompanying pin info
 const getFirstMaps = (db, userId) => {
+  //write query for map data and pins associated with each map
   const query = `
       SELECT maps.*, users.name as user_name, array_agg(pins.lat || ',' || pins.lng) as markers
        FROM maps
@@ -16,19 +17,22 @@ const getFirstMaps = (db, userId) => {
     .then(data => {
       const mapsArray = data.rows;
       console.log("MAPS ARRAY", mapsArray);
-      //executes other function which is asynchronous and returns a promise
+      //execute function to get favourites, which is asynchronous and returns a promise
       return getMyFavourites(db, userId)
-      //wait until we have the favourites data
-      //to check if looped map is inside of favData
+      //once we have favourites data, then we can check in mapsArray loop if map is inside of favData array
       .then((favData) => {
         console.log("FAV DATA: ", favData)
         for (let map of mapsArray) {
+          //if map.markers is null, set markersQuery key to equal empty string
+          //and map.markers to equal empty array
           if (!map.markers[0]) {
             map.markersQuery = "";
             map.markers = [];
+            //else, give markersQuery key the value of a string to be passed into src in ejs template
           } else {
             map.markersQuery = map.markers.map((m) => `markers=${m}`).join(`&`) + "&";
           }
+          //make myFavourites key in map object and set boolean value
           map.myFavourites = favData.includes(map.id)
         }
         console.log("Mutated maps array", mapsArray)
@@ -41,12 +45,15 @@ const getFirstMaps = (db, userId) => {
 };
 
 const getMyFavourites = (db, userId) => {
+  //write query for favourites where userId is set in cookie
   const query = `
   SELECT * FROM favourites
   WHERE user_id = ${userId}
   ;`;
   return db
   .query(query)
+  //take only map id out of data.rows objects, put into array, and return array of map id's which are liked by this user
+  //return a promise
   .then(data => {
     const favourites = data.rows;
     const mapIds = favourites.map(fav => fav.map_id)
