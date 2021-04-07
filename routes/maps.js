393f4f3b26b06656027280fd8f45b7aec862e8e2
code-driving <tittan7@gmail.com>
2021-04-07 00:43:01 +0000

    Change input to textarea and start creating basic check

--- routes/maps.js
+++ routes/maps.js
@@ -1,10 +1,9 @@
-
-const { Template } = require('ejs');
+const { Template } = require("ejs");
 const express = require("express");
 const router = express.Router();
-const dbFns = require('../queries/maps_db');
+const dbFns = require("../queries/maps_db");
 const dbUserFns = require("../queries/users_db");
-const { getMapById, getPins } = require('../queries/maps_db');
+const { getMapById, getPins } = require("../queries/maps_db");
 
 module.exports = (db, apiKey) => {
   // // GET /maps/
@@ -21,63 +20,37 @@ module.exports = (db, apiKey) => {
   //     });
   // });
 
-
+//show form for map creation
   router.get("/new", (req, res) => {
     console.log("==> GET /maps/create -- Create new map");
 
     const userId = req.session.user_id;
 
-    dbUserFns.getUserById(db, userId)
-    .then(user => {
-      const templateVars = {
-        mapId: req.body.id,
-        user,
-        title: req.body.title,
-        description: req.body.description,
-        zoom: req.body.zoom,
-        latitude: req.body.latitude,
-        longitude: req.body.longitude
-      }
-      res.render('create', templateVars);
-    })
-    .catch(error => {
-      res.send('error');
-    })
+    dbUserFns
+      .getUserById(db, userId)
+      .then((user) => {
+        const templateVars = {
+          mapId: req.body.id,
+          user,
+          title: req.body.title,
+          description: req.body.description,
+          zoom: req.body.zoom,
+          latitude: req.body.latitude,
+          longitude: req.body.longitude,
+        };
+        res.render("create", templateVars);
+      })
+      .catch((error) => {
+        res.status(500).json(error);
+      });
   });
 
-  // POST /maps/create -- Create a new map
-  // router.post("/", (req, res) => {
-  //   console.log("==> POST /maps/create -- Create new map");
-  //   const mapId = req.body.id;
-  //   const user = req.session.user_id;
-  //    // if (!user) {
-  //   //   res.redirect("/login");
-  //   //   return;
-  //   // }
-  //   const maps = {
-  //     mapId: req.body.id,
-  //     user: 1,
-  //     title: req.body.title,
-  //     description: req.body.description,
-  //     zoom: req.body.zoom,
-  //     latitude: req.body.latitude,
-  //     longitude: req.body.longitude
-  //   }
-  //   console.log(dbFns.createMap(db, maps));
-
-  //   const templateVars = {
-  //     map: maps,
-  //     pins: []
-  //   };
-  //   res.render('map_show', templateVars);
-  // });
-
-router.post("/", (req, res) => {
+//Create a new map
+  router.post("/", (req, res) => {
     console.log("==> POST /maps/create -- Create new map");
     const mapId = req.body.id;
     const user = req.session.user_id;
 
-console.log("user==>", user);
     const maps = {
       mapId: req.body.id,
       user: user,
@@ -85,36 +58,33 @@ console.log("user==>", user);
       description: req.body.description,
       zoom: req.body.zoom,
       latitude: req.body.latitude,
-      longitude: req.body.longitude
-    }
+      longitude: req.body.longitude,
+    };
 
     Promise.all([dbUserFns.getUserById(db, user), dbFns.createMap(db, maps)])
-    .then(([user, maps]) => {
-      console.log("user==>", user);
-      const templateVars = { user, map: maps, pins: [] };
-      res.render("map_show", templateVars);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    })
+      .then(([user, maps]) => {
+        const templateVars = { user, map: maps, pins: [] };
+        res.render("map_show", templateVars);
+      })
+      .catch((error) => {
+        res.status(500).json(error);
+      });
   });
 
   // GET /maps/:map_id  -- Display a map by id
-  router.get('/:map_id', (req, res) => {
-    console.log('==> GET /maps/:map_id  -- Display a map by id');
+  router.get("/:map_id", (req, res) => {
+    console.log("==> GET /maps/:map_id  -- Display a map by id");
 
     const mapID = req.params.map_id;
     let templateVars = {};
-    getMapById(db, mapID)
-      .then((mapObj) => {
-        templateVars.map = mapObj;
-        getPins(db, mapID)
-        .then((pinsArray) => {
-          templateVars.pins = pinsArray;
-          templateVars.apiKey = apiKey;
-          res.render('map_show', templateVars);
-        })
+    getMapById(db, mapID).then((mapObj) => {
+      templateVars.map = mapObj;
+      getPins(db, mapID).then((pinsArray) => {
+        templateVars.pins = pinsArray;
+        templateVars.apiKey = apiKey;
+        res.render("map_show", templateVars);
       });
+    });
   });
 
   // POST /maps/:map_id/edit -- Edit a map
@@ -124,17 +94,19 @@ console.log("user==>", user);
 
   // POST /maps/:map_id/delete -- Delete a map
   router.post("/:map_id/delete", (req, res) => {
-
     console.log("==> POST /maps/:map_id/delete -- Delete a map");
 
     const map = req.params.map_id;
     const user = req.session.user_id;
 
-    Promise.all([dbUserFns.getUserById(db, user), dbFns.deleteMap(db, map, user)])
-    .then(res.redirect("/maps"))
-    .catch(error => {
-      res.status(500).json(error);
-    })
+    Promise.all([
+      dbUserFns.getUserById(db, user),
+      dbFns.deleteMap(db, map, user),
+    ])
+      .then(res.redirect("/maps"))
+      .catch((error) => {
+        res.status(500).json(error);
+      });
   });
 
   return router;
