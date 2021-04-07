3bc126ce2395bc3fe5164ee6113cc39cf8fcd13f
KateIsabelle <kate.davis887@gmail.com>
2021-04-07 00:13:53 +0000

    make map 'heart' have liked class, red, if user is logged in and has previously favourited map

--- routes/maps_index.js
+++ routes/maps_index.js
@@ -7,7 +7,8 @@ module.exports = (db, apiKey) => {
 
   router.get('/', (req,res) => {
   console.log("==> GET /maps -- display recent maps");
-  getFirstMaps(db)
+  const userId = req.session.user_id;
+  getFirstMaps(db, userId)
   .then((maps) => {
     let templateVars = {};
     templateVars.maps = maps;
