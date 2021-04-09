cac2798707d9fc1e59c20b458c44335a8bdb2e1c
code-driving <tittan7@gmail.com>
2021-04-09 02:52:14 +0000

    Accepted incoming changes from merge conflict

--- routes/maps.js
+++ routes/maps.js
@@ -9,101 +9,6 @@ const dbUserFns = require("../queries/users_db");
 
 
 module.exports = (db, apiKey) => {
-<<<<<<< HEAD
-  // // GET /maps/
-  // router.get('/', (req, res) => {
-  //   db.query(`SELECT * FROM maps;`)
-  //     .then(data => {
-  //       const maps = data.rows;
-  //       res.json({ maps });
-  //     })
-  //     .catch(err => {
-  //       res
-  //         .status(500)
-  //         .json({ error: err.message });
-  //     });
-  // });
-
-  // GET /maps/:map_id  -- Display a map by id
-  router.get('/:map_id', (req, res) => {
-    const mapID = req.params.map_id;
-    const getMapByIdPromise = dbFns.getMapById(db, mapID);
-    const getPinsPromise = dbFns.getPins(db, mapID);
-
-    Promise.all([getMapByIdPromise, getPinsPromise])
-      .then(([mapObj, pinsArr]) => {
-        const templateVars = {
-          map: mapObj,
-          pins: pinsArr,
-
-          apiKey: apiKey,
-          user_id: req.session.user_id
-
-        };
-        res.render('map_show', templateVars);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-  });
-
-  // POST /maps/:map_id/addpin -- Add a pin
-  router.post("/:map_id/addpin", (req, res) => {
-    const mapID = req.params.map_id;
-    const pin = {
-      map_id: mapID,
-      lat: req.body.coords.lat,
-      lng: req.body.coords.lng,
-      title: req.body.pinInfo.title,
-      description: req.body.pinInfo.description,
-      photo_url: req.body.pinInfo.photo_url
-    }
-
-    pinFns.createPin(db, pin)
-      .catch((err) => console.log(err));
-    res.send('ok');
-  });
-
-
-  // POST /maps/:map_id/delete -- Delete a pin
-  router.post("/:map_id/deletepin", (req, res) => {
-    const mapID = req.params.map_id;
-    const pin = {
-      map_id: mapID,
-      pin_id: req.body.pin_id
-    }
-    pinFns.deletePin(db, pin)
-      .catch((err) => console.log(err));
-    res.send('ok');
-  });
-
-  // POST /maps/:map_id/delete -- Delete a map
-  router.post("/:map_id/delete", (req, res) => {
-
-    const map_id = req.params.id;
-
-    const user = req.session.user_id;
-
-    Promise.all([
-      dbUserFns.getUserById(db, user),
-      dbFns.deleteMap(db, map_id, user),
-    ])
-      .then(res.redirect("/maps"))
-      .catch((error) => {
-        res.status(500).json(error);
-      });
-  });
-
-  // POST /maps/:map_id/edit -- Edit a map
-  router.post("/:map_id/edit", (req, res) => {
-    console.log("==> POST /maps/:map_id/edit -- Edit a map");
-
-
-  });
-  return router;
-};
-=======
 
     // GET /maps/:map_id  -- Display a map by id
     router.get('/:map_id', (req, res) => {
@@ -186,4 +91,3 @@ module.exports = (db, apiKey) => {
 
   };
 
->>>>>>> add5f96fefea6bf1e29bed94e850c59604b30071
