ba4cb851bac017357c79690a6c61f898d4dd6353
KateIsabelle <kate.davis887@gmail.com>
2021-04-09 01:23:35 +0000

    this merge conflict keep happening in maps.js

--- routes/maps.js
+++ routes/maps.js
@@ -10,8 +10,6 @@ const dbUserFns = require("../queries/users_db");
 
 module.exports = (db, apiKey) => {
 
-
-<<<<<<< HEAD
     // GET /maps/:map_id  -- Display a map by id
     router.get('/:map_id', (req, res) => {
       const mapID = req.params.map_id;
@@ -93,100 +91,3 @@ module.exports = (db, apiKey) => {
 
   };
 
-=======
-  //Create a new map
-  router.post("/", (req, res) => {
-    const mapId = req.body.id;
-    const user = req.session.user_id;
-    const maps = {
-      mapId: req.body.id,
-      user: user,
-      title: req.body.title,
-      description: req.body.description,
-      latitude: req.body.latitude,
-      longitude: req.body.longitude,
-    };
-  })
-
-
-
-  // GET /maps/:map_id  -- Display a map by id
-  router.get('/:map_id', (req, res) => {
-    const mapID = req.params.map_id;
-    const getMapByIdPromise = dbFns.getMapById(db, mapID);
-    const getPinsPromise = dbFns.getPins(db, mapID);
-
-    Promise.all([getMapByIdPromise, getPinsPromise])
-      .then(([mapObj, pinsArr]) => {
-        const templateVars = {
-          map: mapObj,
-          pins: pinsArr,
-
-          apiKey: apiKey,
-          user_id: req.session.user_id
-
-        };
-        res.render('map_show', templateVars);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-
-  })
-
-  // POST /maps/:map_id/addpin -- Add a pin
-  router.post("/:map_id/addpin", (req, res) => {
-    const mapID = req.params.map_id;
-    const pin = {
-      map_id: mapID,
-      lat: req.body.coords.lat,
-      lng: req.body.coords.lng,
-      title: req.body.pinInfo.title,
-      description: req.body.pinInfo.description,
-      photo_url: req.body.pinInfo.photo_url
-    }
-
-    pinFns.createPin(db, pin)
-      .catch((err) => console.log(err));
-    res.send('ok');
-  });
-
-
-  // POST /maps/:map_id/delete -- Delete a pin
-  router.post("/:map_id/deletepin", (req, res) => {
-    const mapID = req.params.map_id;
-    const pin = {
-      map_id: mapID,
-      pin_id: req.body.pin_id
-    }
-    pinFns.deletePin(db, pin)
-      .catch((err) => console.log(err));
-    res.send('ok');
-  });
-
-  // POST /maps/:map_id/delete -- Delete a map
-  router.post("/:map_id/delete", (req, res) => {
-
-    const map_id = req.params.id;
-
-    const user = req.session.user_id;
-
-    Promise.all([
-      dbUserFns.getUserById(db, user),
-      dbFns.deleteMap(db, map_id, user),
-    ])
-      .then(res.redirect("/maps"))
-      .catch((error) => {
-        res.status(500).json(error);
-      });
-  });
-
-  // POST /maps/:map_id/edit -- Edit a map
-  router.post("/:map_id/edit", (req, res) => {
-    console.log("==> POST /maps/:map_id/edit -- Edit a map");
-
-
-  });
-  return router;
-};
->>>>>>> feature/sml-map-click-link
