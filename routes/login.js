a926604265c7259a955434aee5f862fc865afff1
code-driving <tittan7@gmail.com>
2021-04-09 02:55:21 +0000

    accept incoming change in login.js

--- routes/login.js
+++ routes/login.js
@@ -5,11 +5,7 @@ module.exports = (db) => {
 
   // set cookie in 'login/:user_id'
   router.get('/', (req, res) => {
-<<<<<<< HEAD
-    req.session.user_id = 1;
-=======
     req.session.user_id = 2;
->>>>>>> add5f96fefea6bf1e29bed94e850c59604b30071
     res.redirect('/maps');
   });
   return router;
