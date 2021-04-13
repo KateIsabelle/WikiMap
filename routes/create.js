e3c6b3731310c8bc6fb9331873bb49ce9e3e3824
Kate Davis <kate.davis887@gmail.com>
2021-04-13 08:47:31 -0400

    remove pseudocode from create.js

--- routes/create.js
+++ routes/create.js
@@ -23,8 +23,7 @@ module.exports = (db, apiKey) => {
       });
   });
 
-//Kate's changes. We need to see what is missing
-  //Create a new map
+  //Create a new map: post info from user form and redirect to /maps/:id
   router.post("/", (req, res) => {
 
     const user = req.session.user_id;
@@ -32,7 +31,7 @@ module.exports = (db, apiKey) => {
     const maps = {
       user: user,
       title: req.body.title,
-      description: req.body["map-description"],
+      description: req.body.description,
       latitude: req.body.latitude,
       longitude: req.body.longitude,
     };
@@ -45,30 +44,7 @@ module.exports = (db, apiKey) => {
         res.status(500).json(error);
       });
   });
-  //My original.
-  // router.post("/", (req, res) => {
-  //   const mapId = req.body.id;
-  //   const user = req.session.user_id;
 
-  //   const maps = {
-  //     mapId: req.body.id,
-  //     user: user,
-  //     title: req.body.title,
-  //     description: req.body.description,
-  //     latitude: req.body.latitude,
-  //     longitude: req.body.longitude,
-  //   };
-
-  //   Promise.all([dbUserFns.getUserById(db, user), dbFns.createMap(db, maps)])
-  //     .then(([user, maps]) => {
-  //       const templateVars = { user, map: maps, pins: [] };
-  //       res.render("maps_show", templateVars);
-  //     })
-  //     .catch((error) => {
-  //       console.log("error ==>", error);
-  //       res.status(500).json(error);
-  //     });
-  // });
   return router;
 }
 
